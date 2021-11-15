acfcb037984553a12e305522da2931ba03b13ad3
Xao <divxyng.edu@gmail.com>
2021-11-15 09:04:35 -0600

    Addec config to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    resources :lists, except: [:new, :edit]
+    resources :list_items, only: [:create, :update, :destroy]
+  end
+
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#logout'
+  get '/signup', to: 'registrations#create'
 end
